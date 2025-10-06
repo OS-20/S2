02ab4e683013cc38e3e2bcdaa3b676d72ebea608
OS-20 <insearchofdreams12@gmail.com>
2025-10-06 22:32:26 +0530

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,36 +1,81 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<dirent.h>
 
-void listCmd(char *opt, char *dir) {
-    DIR *dp = opendir(dir);
-    if (!dp) { perror("dir"); return; }
-    struct dirent *d; int n = 0;
-    if (!strcmp(opt, "f"))
-        while ((d = readdir(dp))) printf("%s\n", d->d_name);
-    else if (!strcmp(opt, "n")) {
-        while ((d = readdir(dp))) n++;
-        printf("Total entries: %d\n", n);
-    } else printf("Usage: list f dir | list n dir\n");
+void make_toks(char *s, char *tok[]) {
+    int i = 0;
+    char *p;
+    p = strtok(s, " ");
+    while(p != NULL) {
+        tok[i++] = p;
+        p = strtok(NULL, " ");
+    }
+    tok[i] = NULL;
+}
+
+void list(char *dn, char op) {
+    DIR *dp;
+    struct dirent *entry;
+    int dc = 0, fc = 0;
+    dp = opendir(dn);
+    if(dp == NULL) {
+        printf("file %s not found\n", dn);
+        return;
+    }
+    switch(op) {
+        case 'f':
+            while(entry = readdir(dp)) {
+                if(entry->d_type == DT_REG)
+                    printf("%s\n", entry->d_name);
+            }
+            break;
+        case 'n':
+            while(entry = readdir(dp)) {
+                if(entry->d_type == DT_DIR) dc++;
+                if(entry->d_type == DT_REG) fc++;
+            }
+            printf("%dDir(s)\t %d File(s)\n", dc, fc);
+            break;
+        case 'i':
+            while(entry = readdir(dp)) {
+                if(entry->d_type == DT_REG)
+                    printf("%s\t%lu\n", entry->d_name, entry->d_ino);
+            }
+            break;
+    }
     closedir(dp);
 }
 
-int main() {
-    char in[100], *a[10];
-    while (1) {
-        printf("myshell$ ");
-        if (!fgets(in, sizeof(in), stdin)) break;
-        if (!(a[0] = strtok(in, " \t\n"))) continue;
-        int i = 0; while ((a[++i] = strtok(NULL, " \t\n")));
-        if (!strcmp(a[0], "exit")) break;
-        else if (!strcmp(a[0], "list")) {
-            if (a[1] && a[2]) listCmd(a[1], a[2]);
-            else printf("Usage: list f dir | list n dir\n");
-        } else if (fork() == 0) {
-            execvp(a[0], a); perror("cmd"); exit(1);
-        } else wait(NULL);
+int main()
+{
+    char buff[80],*args[10];
+    int pid;
+    while(1)
+    {
+        printf("Myshell$");
+        fflush(stdin);
+        fgets(buff,80,stdin);
+        buff[strlen(buff)-1]='\0';
+        make_toks(buff,args);
+        if(strcmp(args[0],"list")==0)
+            list(args[2],args[1][0]);
+        else
+        {
+            pid=fork();
+            if(pid>0)
+                wait(NULL);
+            else
+            {
+                if((execvp(args[0],args)==-1))
+                    printf("Bad Command\n");
+            }
+        }
     }
+    return 0;
 }
